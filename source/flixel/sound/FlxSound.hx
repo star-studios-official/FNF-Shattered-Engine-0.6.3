09957b98a69f3d3e9daf1ea2ef31778f9e33bbfb
natesway <64743334+natesway@users.noreply.github.com>
2023-09-07 13:34:22 -0400

    Update FlxSound.hx

--- source/flixel/sound/FlxSound.hx
+++ source/flixel/sound/FlxSound.hx
@@ -8,6 +8,7 @@ import flash.media.SoundTransform;
 import flash.net.URLRequest;
 import flixel.FlxBasic;
 import flixel.tweens.FlxTween;
+import flixel.math.FlxMath
 
 
 import flixel.math.FlxPoint;
